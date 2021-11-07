109cc659548750c269ab4145f04c7a9c8da8fd57
unityproject1 <93753776+unityproject1@users.noreply.github.com>
2021-11-07 18:46:46 +0900

    Add files via upload

---
+++ Attribute.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class ReadOnly : PropertyAttribute
+{
+    
+}
+
+[CustomPropertyDrawer(typeof(ReadOnly))]
+public class ReadOnlyDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.BeginDisabledGroup(true);
+        EditorGUI.PropertyField(position, property, label, true);
+        EditorGUI.EndDisabledGroup();
+    }
+}
