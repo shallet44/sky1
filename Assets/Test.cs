956bd78f05bd0bc312de52983f790a8fb9c59b3f
shallet44 <66909413+shallet44@users.noreply.github.com>
2021-11-07 19:06:15 +0900

    Update Test.cs

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -8,7 +8,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("너무 배고파요")
+        Debug.Log("안녕하세요")
     }
 
     // Update is called once per frame
