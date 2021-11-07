6593a0a12f1ad20b87c710dc08ec01cf4e8f56c7
정하늘 <gksmf2787@naver.com>
2021-11-07 18:39:30 +0900

    dfs

---
+++ Assets/Test.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("너무 배고파요")
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
